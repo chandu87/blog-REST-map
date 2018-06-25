c8521d4a2c76894577ab2f73cd0e52e5fe523cbb
chandu <chandrasr.palle@gmail.com>
2018-06-25 12:24:32 +0200

    data displayed on blog route from db

--- app.js
+++ app.js
@@ -1,37 +1,45 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 //Set View engine
-app.set('view engine',"ejs");
-app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 mongoose.connect("mongodb://localhost/blog_app");
 app.use(express.static("public"));
 
+//Mongoose model and config
 const blogSchema = new mongoose.Schema({
-        name: String,
-        image: String,
-        body: String,
-        created: {type : Date, default : Date.now} 
-
+  name: String,
+  image: String,
+  body: String,
+  created: { type: Date, default: Date.now }
 });
 
 let Blog = mongoose.model("Blog", blogSchema);
 
-Blog.create({
-    name: "Roasted MIX",
-    image: "https://unsplash.com/photos/eY8sI1uqNuY",
-    body: "Wonderful roasted almonds. Dry roasting with sea salt",
-    created: Date.now()
+// Blog.create({
+//     name: "Roasted MIX",
+//     image: "https://images.unsplash.com/photo-1529381301888-8856b2080591?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=4f0c326b2d490bd5d9eebb4b970f731f&auto=format&fit=crop&w=3144&q=80",
+//     body: "Wonderful roasted almonds. Dry roasting with sea salt",
+//     created: Date.now()
+// });
+
+//RESTFUL ROUTES
+app.get("/", function(req, res) {
+  res.redirect("/blogs");
+});
+app.get("/blogs", function(req, res) {
+  Blog.find({}, function(err, data) {
+    if (err) {
+      console.log("There is Some error in Finding blogs data", err);
+    } else {
+      res.render("blogs.ejs",{blogs : data});
+    }
+  });
 });
 
-
-app.get("/", function(req, res){
-    res.send("Hello World!!");
-});
-
-app.listen(3000, function(){
-    console.log("Server Started : 3000");
+app.listen(3000, function() {
+  console.log("Server Started : 3000");
 });
