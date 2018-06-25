1ab8ff1156daaed38986efc38c5043ec13286925
chandu <chandrasr.palle@gmail.com>
2018-06-25 11:59:03 +0200

    db schema and model created

--- app.js
+++ app.js
@@ -8,8 +8,29 @@ const mongoose = require('mongoose');
 app.set('view engine',"ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 mongoose.connect("mongodb://localhost/blog_app");
-// app.use("static","publc")
+app.use(express.static("public"));
 
+const blogSchema = new mongoose.Schema({
+        name: String,
+        image: String,
+        body: String,
+        created: {type : Date, default : Date.now} 
+
+});
+
+let Blog = mongoose.model("Blog", blogSchema);
+
+Blog.create({
+    name: "Roasted MIX",
+    image: "https://unsplash.com/photos/eY8sI1uqNuY",
+    body: "Wonderful roasted almonds. Dry roasting with sea salt",
+    created: Date.now()
+});
+
+
+app.get("/", function(req, res){
+    res.send("Hello World!!");
+});
 
 app.listen(3000, function(){
     console.log("Server Started : 3000");
